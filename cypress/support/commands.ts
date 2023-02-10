1ec3129cde64a33d076abb4b8c42df7b405f23c1
João Melo <jopcmelo@gmail.com>
2023-02-10 18:55:49 -0300

    fix: try to fix husky hooks

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+/* eslint-disable @typescript-eslint/no-empty-interface */
 /// <reference types="cypress" />
 // ***********************************************
 // This example commands.ts shows you how to
